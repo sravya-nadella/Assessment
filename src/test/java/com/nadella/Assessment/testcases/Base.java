9a5fabc6b654c41e0613fe2f8a978f796e72687c
sravya-nadella <nsravya@gmail.com>
2021-09-27 09:38:46 -0400

    changed implicit wait

--- src/test/java/com/nadella/Assessment/testcases/Base.java
+++ src/test/java/com/nadella/Assessment/testcases/Base.java
@@ -16,7 +16,7 @@ public class Base {
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
 	}
